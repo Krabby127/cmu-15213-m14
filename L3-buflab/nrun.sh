72601204d0400b34a95a8a3130b0e1fc905cda2a
Nikita Chepanov <nchepano@unix5.andrew.cmu.edu>
2014-06-17 16:50:11 -0400

    buflab done

--- L3-buflab/nrun.sh
+++ L3-buflab/nrun.sh
@@ -6,8 +6,14 @@ breaks="$1.breaks"
 ./hex2raw < $1 > $raw
 
 touch $breaks
-echo "break getbuf" > $breaks
+
+echo "break getbufn" > $breaks
+echo "break testn"  >>$breaks
+echo 'display/5i $eip' >> $breaks
+echo 'display/4x $ebp' >> $breaks
+echo 'display/4x $esp' >> $breaks
 echo "set step-mode on" >> $breaks
+echo 'display/x ($ebp+4)' >> $breaks
 echo "run -n -t nchepano < $raw" >>$breaks
 #echo "layout regs" >> $breaks
 gdb -x $breaks bufbomb
