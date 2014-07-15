4e101f4e1376e0acbe34a42ed8d22a1b177e0cb4
Nikita Chepanov <nchepano@baskingshark.ics.cs.cmu.edu>
2014-07-14 23:21:11 -0400

    Compiles, but of course doesn't work

--- L6-malloclab/mm.c
+++ L6-malloclab/mm.c
@@ -2,9 +2,18 @@
  * mm.c
  * hbovik - Harry Bovik
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a full description of your solution.
- */
+            --> block ptr
+            | 
+--┌---------┬----------┬---------┬------
+  |         |          |         |
+  |  header |   data   |  footer | header ...
+  | 8 bytes |          | 8 bytes |
+--┴---------┴----------┴---------┴-----
+
+  <-------------size------------->
+
+
+*/
 
 #include <assert.h>
 #include <stdio.h>
@@ -46,10 +55,42 @@
 #define checkheap(...)
 #endif
 
+// Macros
+
+/* Word size in bytes */
+#define WSIZE       8
+/* Memory allignmnet in bytes */
+#define ALIGNMENT   WSIZE       
+/* expand heap by this, in bytes */
+#define CHUNKSIZE   (1<<12)
+
+/* lower bit of header/footer */
+#define FREE        0
+#define ALLOCED     1
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+/* cast pointer to size_t* */
+#define PTR(p) ((size_t*)(p))
+
+/* max value */
+#define MAX(x, y)   ((x) > (y) ? (x) : (y))
+
 /*
  *  Helper functions
  *  ----------------
  */
+
+// Get 8 bytes by the pointer
+static inline size_t get(const char* ptr) {
+    return *PTR(ptr);
+}
+
+// Set 8 bytes by the pointer
+static inline size_t put(char* ptr, size_t val) {
+    return *PTR(ptr) = val;
+}
 
 // Align p to a multiple of w bytes
 static inline void* align(const void const* p, unsigned char w) {
@@ -75,55 +116,46 @@ static int in_heap(const void* p) {
  *  size in multiples of 4 bytes.
  */
 
-// Return the size of the given block in multiples of the word size
-static inline unsigned int block_size(const uint32_t* block) {
+// Return the size of the given block bytes
+static inline size_t block_size(const char* block) {
     REQUIRES(block != NULL);
     REQUIRES(in_heap(block));
-
-    return (block[0] & 0x3FFFFFFF);
+    return get(block - WSIZE) & ~0x7;
 }
 
 // Return true if the block is free, false otherwise
-static inline int block_free(const uint32_t* block) {
+static inline size_t is_block_free(const char* block) {
     REQUIRES(block != NULL);
     REQUIRES(in_heap(block));
 
-    return !(block[0] & 0x40000000);
+    return !(get(block - WSIZE) & 0x1);
 }
 
 // Mark the given block as free(1)/alloced(0) by marking the header and footer.
-static inline void block_mark(uint32_t* block, int free) {
+static inline void mark_block(char* block, int free, size_t size) {
     REQUIRES(block != NULL);
-    REQUIRES(in_heap(block));
+    REQUIRES(in_heap    (block));
 
-    unsigned int next = block_size(block) + 1;
-    block[0] = free ? block[0] & (int) 0xBFFFFFFF : block[0] | 0x40000000;
-    block[next] = block[0];
+    size_t val = size | free;
+    put(block - WSIZE, val);
+    put(block + size , val);
 }
 
-// Return a pointer to the memory malloc should return
-static inline uint32_t* block_mem(uint32_t* const block) {
-    REQUIRES(block != NULL);
-    REQUIRES(in_heap(block));
-    REQUIRES(aligned(block + 1));
-
-    return block + 1;
-}
 
 // Return the header to the previous block
-static inline uint32_t* block_prev(uint32_t* const block) {
+static inline char* block_prev(char* const block) {
     REQUIRES(block != NULL);
     REQUIRES(in_heap(block));
 
-    return block - block_size(block - 1) - 2;
+    return block - block_size(block) - 2*WSIZE;
 }
 
 // Return the header to the next block
-static inline uint32_t* block_next(uint32_t* const block) {
+static inline char* block_next(char* const block) {
     REQUIRES(block != NULL);
     REQUIRES(in_heap(block));
 
-    return block + block_size(block) + 2;
+    return block + block_size(block) + WSIZE;
 }
 
 
@@ -134,50 +166,216 @@ static inline uint32_t* block_next(uint32_t* const block) {
  */
 
 /*
+ * Global vars
+ */
+static char *heap_listp;
+
+/*
+ * My helper function headers
+ */
+
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t bp);
+static void put_block(void* bp, size_t size);
+/*
  * Initialize: return -1 on error, 0 on success.
  */
 int mm_init(void) {
+    char* prologue;
+    heap_listp = mem_sbrk(4*WSIZE);
+    if (heap_listp == (void *)-1) return -1;
+
+    /* allignemnt, size: WSIZE */
+    put(heap_listp, 0);
+
+    /* put prologue, size: 2*WSIZE */
+    prologue = heap_listp + WSIZE;
+    mark_block(prologue, 2*WSIZE, 1);
+
+    /* epilogue, size: WSIZE, alloced*/
+    put(prologue + 2*WSIZE, 0x1);
+
+    /* point to the footer of prologue */
+    heap_listp += 2*WSIZE;
+
+    if (extend_heap(CHUNKSIZE/ WSIZE) == NULL)
+        return -1;
     return 0;
 }
 
+/*
+ * Extend heap by CHUNKSIZE
+ */
+static void *extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+
+    size = words*WSIZE; 
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    mark_block(bp, size, FREE);
+    put(block_next(bp), 0x1);
+    return coalesce(bp);
+
+}
+
+static void *coalesce(void *bp){
+    size_t prev_alloc = is_block_free(block_prev(bp));
+    size_t next_alloc = is_block_free(block_next(bp));
+    size_t size = block_size(bp);
+
+    if (prev_alloc && next_alloc) {
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc) {
+        size += block_size(block_next(bp));
+        mark_block(bp, size, FREE);
+    }
+
+    else if (!prev_alloc && next_alloc) {
+        size += block_size(block_prev(bp));
+        bp = block_prev(bp);
+        mark_block(bp, size, FREE);
+    }
+
+    else {
+        size += block_size(block_prev(bp)) + 
+            block_size(block_next(bp));
+        bp = block_prev(bp);
+        mark_block(bp, size, FREE);
+    }
+    return bp;
+}
+
+static void* find_fit(size_t size) {
+    char* ptr = heap_listp + WSIZE;
+    while (ptr != NULL)  {
+        if (is_block_free(ptr) && block_size(ptr) >= size) break;
+        ptr = block_next(ptr);
+    }
+    return ptr;
+}
+
+static void put_block(void* bp, size_t size){
+    size_t old_size = block_size(bp);
+    if (size < old_size) {
+        char* free = (char*)bp + size + WSIZE;
+        mark_block(free, old_size - size, FREE);
+    } 
+    mark_block(bp, size, ALLOCED);
+    checkheap(1);  // Let's make sure the heap is ok!
+}
 /*
  * malloc
  */
 void *malloc (size_t size) {
     checkheap(1);  // Let's make sure the heap is ok!
-    size = size;
-    return NULL;
+    size_t asize;
+    size_t extendsize;
+    char *bp;
+    if (size == 0)
+        return NULL;
+
+    if (size <= WSIZE)
+        asize = 3*WSIZE;
+    else
+        asize = ALIGN(size + 2*WSIZE); 
+
+    /* Search the free list for a fit */
+    if ((bp = find_fit(asize)) != NULL) {
+        put_block(bp, asize);
+        return bp;
+    }
+
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    put_block(bp, asize);
+    checkheap(1);  // Let's make sure the heap is ok!
+    return bp;
 }
 
 /*
  * free
  */
-void free (void *ptr) {
-    if (ptr == NULL) {
-        return;
-    }
+void free (void *bp) {
+    size_t size = block_size(bp);
+    mark_block(bp, size, FREE);
+    coalesce(bp);
+    checkheap(1);  // Let's make sure the heap is ok!
 }
 
 /*
  * realloc - you may want to look at mm-naive.c
  */
 void *realloc(void *oldptr, size_t size) {
-    oldptr = oldptr;
-    size = size;
-    return NULL;
+    size_t oldsize;
+    void *newptr;
+
+    if (size == 0) {
+        free(oldptr);
+        return NULL;
+    }
+
+    if (oldptr == NULL) {
+        return malloc(size);
+    }
+
+    newptr = malloc(size);
+
+    if (!newptr) {
+        return 0;
+    }
+
+    oldsize = block_size(oldptr);
+    if (size < oldsize) oldsize = size;
+    memcpy(newptr, oldptr, oldsize);
+
+    free(oldptr);
+
+    return newptr;
 }
 
 /*
  * calloc - you may want to look at mm-naive.c
  */
 void *calloc (size_t nmemb, size_t size) {
-    nmemb = nmemb;
-    size = size;
-    return NULL;
+    size_t bytes = nmemb * size;
+    void* newptr;
+    newptr = malloc(bytes);
+    memset(newptr, 0, bytes);
+
+    return newptr;
 }
 
 // Returns 0 if no errors were found, otherwise returns the error
 int mm_checkheap(int verbose) {
-    verbose = verbose;
+    char* ptr = heap_listp + WSIZE;
+    size_t size = 0, header = 0, footer = 0;
+    int free = 0;
+    while (ptr != NULL) {
+        if (verbose) {
+            printf("[%p+%zu,%s]->",ptr, size, free?"free":"allo");
+        }
+        if (!aligned(ptr)) {
+            printf("Block pointer %p isn't aligned\n", ptr);
+            return 1;
+        }
+        if (!in_heap(ptr)) {
+            printf("Block pointer %p isn't in heap\n", ptr);
+            return 1;
+        }
+        size = block_size(ptr);
+        free = is_block_free(ptr);
+        header = get(ptr - WSIZE);
+        footer = get(ptr + size);
+        if (header != footer) {
+            printf("Header and footer doesn't match: %p\n", ptr );
+            return 1;   
+        }
+        ptr = block_next(ptr);
+    }
     return 0;
 }
